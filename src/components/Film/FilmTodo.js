61fd0a1795e7a3bb935cf50d7e789fd8d953046b
KyleOrr676 <Kyle.Orr@hotmail.com>
2021-12-02 09:10:34 +0000

    kyle commit

--- src/components/Film/FilmTodo.js
+++ src/components/Film/FilmTodo.js
@@ -1,32 +1,29 @@
-import React from 'react';
+import React from "react";
 function FilmTodo(props) {
+  let filmTitle = props.filmTodo.title;
 
-    let filmTitle = props.filmTodo.title;
-
-    return (
-        <div className="filmTodo">
-            <div className="row">
-                <div className="col-lg-4"></div>
-                <div className="col-lg-4">
-                    <div className="row">
-                        <div className="col-lg-2"></div>
-                        <div className="col-lg-8">
-                            <div className="card border-primary w-100">
-                                <div className="actorName">
-                                    <h5>
-                                        Title: {filmTitle}
-                                    </h5>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-2"></div>
-                    </div>
+  return (
+    <div className="filmTodo">
+      <div className="row">
+        <div className="col-lg-4"></div>
+        <div className="col-lg-4">
+          <div className="row">
+            <div className="col-lg-2"></div>
+            <div className="col-lg-8">
+              <div className="card border-primary w-100">
+                <div className="actorName">
+                  <h5>Title: {filmTitle}</h5>
                 </div>
-                <div className="col-lg-4"></div>
+              </div>
             </div>
-            <hr></hr>
+            <div className="col-lg-2"></div>
+          </div>
         </div>
-    );
+        <div className="col-lg-4"></div>
+      </div>
+      <hr></hr>
+    </div>
+  );
 }
 
 export default FilmTodo;
